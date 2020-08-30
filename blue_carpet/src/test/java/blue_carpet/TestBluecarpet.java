14f66d7d7df534a53c7d931bf02c3482949dcf49
matrixreturns <42109987+matrixreturns@users.noreply.github.com>
2020-08-30 14:34:07 +0800

    first commit of another local repo to GitHub called as apple_juice

---
+++ blue_carpet/src/test/java/blue_carpet/TestBluecarpet.java
@@ -0,0 +1,13 @@
+package blue_carpet;
+
+import org.testng.annotations.Test;
+
+public class TestBluecarpet {
+	
+	@Test
+	public void testMyMethod()
+	{
+		System.out.println("my method passed");
+	}
+
+}
